a5901ea064169bebd6d94545f5aba4b2532d3074
Augustin Laurent <augustin.lrt2@icloud.com>
2025-07-20 11:25:39 +0200

    fix(pdfserviceimpl_quality): reorded modifiers as pointed by sonar

--- src/test/java/application/PDFServiceImplTest.java
+++ src/test/java/application/PDFServiceImplTest.java
@@ -21,7 +21,7 @@ class PDFServiceImplTest {
     private PDFDocument document;
     private PDFService pdfService;
 
-    private final static String FALLBACK_PDF_CONTENT = "%PDF-1.4\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj 2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj 3 0 obj<</Type/Page/Parent 2 0 R>>endobj xref 0 4 0000000000 65535 f 0000000010 00000 n 0000000053 00000 n 0000000125 00000 n trailer<</Size 4/Root 1 0 R>> startxref 173 %%EOF";
+    private static final String FALLBACK_PDF_CONTENT = "%PDF-1.4\n1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj 2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj 3 0 obj<</Type/Page/Parent 2 0 R>>endobj xref 0 4 0000000000 65535 f 0000000010 00000 n 0000000053 00000 n 0000000125 00000 n trailer<</Size 4/Root 1 0 R>> startxref 173 %%EOF";
 
     @BeforeEach
     void setUp() {
